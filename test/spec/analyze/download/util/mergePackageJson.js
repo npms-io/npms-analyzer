e356eeda8b183ef4b750969c1f0ac4fbc78683c7
André Cruz <andremiguelcruz@msn.com>
2016-04-14 23:55:40 +0100

    Add sketch tests for download utils.

---
+++ test/spec/analyze/download/util/mergePackageJson.js
@@ -0,0 +1,9 @@
+'use strict';
+
+describe('mergePackageJson', () => {
+    it('should merge the files, prefering the passed one');
+
+    it('should merge the files, prefering the downloaded one');
+
+    it('should deal with broken downloaded package.json files');
+});
---
+++ test/spec/analyze/download/util/untar.js
@@ -0,0 +1,11 @@
+'use strict';
+
+describe('untar', () => {
+    it('should decompress a tar.gz archive file');
+
+    it('should chmod 0777 extracted files recursively');
+
+    it('should deal with malformed archives');
+
+    it('should delete the archive file, even if the extraction failed');
+});
