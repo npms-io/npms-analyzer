c31630294180d92972158726acfea9922a3ca859
André Cruz <andremiguelcruz@msn.com>
2016-04-18 23:19:48 +0100

    Add download/index skeleton tests.

---
+++ test/spec/analyze/download/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+describe('index', () => {
+    it('should find an appropriate downloader, passing in the correct options');
+
+    it('should call downloader with the temporary folder');
+
+    it('should delete the temporary folder on failure');
+});
