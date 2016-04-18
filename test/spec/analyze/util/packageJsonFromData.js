b1b4eb4439535e3dddea8ab9683d60115ab16420
André Cruz <andremiguelcruz@msn.com>
2016-04-19 00:25:59 +0100

    Add packageJsonFromData tests.

---
+++ test/spec/analyze/util/packageJsonFromData.js
@@ -0,0 +1,122 @@
+'use strict';
+
+const expect = require('chai').expect;
+const packageJsonFromData = require(`${process.cwd()}/lib/analyze/util/packageJsonFromData`);
+
+describe('packageJsonFromData', () => {
+    it('should return the latest package json', () => {
+        const packageJson = packageJsonFromData('foo', {
+            name: 'foo',
+            'dist-tags': { latest: '1.0.0' },
+            versions: {
+                '0.1.0': { name: 'foo', version: '0.1.0' },
+                '1.0.0': { name: 'foo', version: '1.0.0' },
+                '2.0.0': { name: 'foo', version: '2.0.0' },
+            },
+        });
+
+        expect(packageJson).to.be.an('object');
+        expect(packageJson.name).to.equal('foo');
+        expect(packageJson.version).to.equal('1.0.0');
+    });
+
+    it('should not crash if there\'s no latest dist-tags', () => {
+        [
+            {
+                name: 'foo',
+                versions: {
+                    '1.0.0': { name: 'foo', version: '1.0.0' },
+                    '2.0.0': { name: 'foo', version: '2.0.0' },
+                },
+            },
+            {
+                name: 'foo',
+                'dist-tags': {},
+                versions: {
+                    '1.0.0': { name: 'foo', version: '1.0.0' },
+                    '2.0.0': { name: 'foo', version: '2.0.0' },
+                },
+            },
+        ].forEach((data) => {
+            const packageJson = packageJsonFromData('foo', data);
+
+            expect(packageJson).to.be.an('object');
+            expect(packageJson.name).to.equal('foo');
+            expect(packageJson.version).to.equal('0.0.1');
+        });
+    });
+
+    it('should not crash if there\'s no matching version', () => {
+        let packageJson;
+
+        packageJson = packageJsonFromData('foo', {
+            name: 'foo',
+            'dist-tags': { latest: '1.0.0' },
+        });
+
+        expect(packageJson).to.be.an('object');
+        expect(packageJson.name).to.equal('foo');
+        expect(packageJson.version).to.equal('1.0.0');
+
+        packageJson = packageJsonFromData('foo', {
+            name: 'foo',
+            'dist-tags': { latest: '1.0.0' },
+            versions: {
+                '2.0.0': { name: 'foo', version: '2.0.0' },
+            },
+        });
+
+        expect(packageJson).to.be.an('object');
+        expect(packageJson.name).to.equal('foo');
+        expect(packageJson.version).to.equal('1.0.0');
+    });
+
+    it('should fail if names mismatch', () => {
+        expect(() => packageJsonFromData('foo', {
+            name: 'bar',
+            'dist-tags': { latest: '1.0.0' },
+            versions: {
+                '1.0.0': { name: 'foo', version: '2.0.0' },
+            },
+        })).to.throw(/name mismatch/i);
+
+        expect(() => packageJsonFromData('foo', {
+            name: 'foo',
+            'dist-tags': { latest: '1.0.0' },
+            versions: {
+                '1.0.0': { name: 'bar', version: '2.0.0' },
+            },
+        })).to.throw(/name mismatch/i);
+
+        expect(() => packageJsonFromData('bar', {
+            name: 'foo',
+            'dist-tags': { latest: '1.0.0' },
+            versions: {
+                '1.0.0': { name: 'foo', version: '2.0.0' },
+            },
+        })).to.throw(/name mismatch/i);
+    });
+
+    it('should fail if versions mismatch', () => {
+        expect(() => packageJsonFromData('foo', {
+            name: 'foo',
+            'dist-tags': { latest: '1.0.0' },
+            versions: {
+                '1.0.0': { name: 'foo', version: '2.0.0' },
+            },
+        })).to.throw(/version mismatch/i);
+    });
+
+    it('should normalize package json with normalize-package-data', () => {
+        const packageJson = packageJsonFromData('foo', {
+            name: 'foo',
+            'dist-tags': { latest: '1.0.0' },
+            versions: {
+                '1.0.0': { name: 'foo', version: '1.0.0' },
+            },
+        });
+
+        expect(packageJson).to.be.an('object');
+        expect(packageJson.readme).to.equal('ERROR: No README data found!');
+    });
+});
