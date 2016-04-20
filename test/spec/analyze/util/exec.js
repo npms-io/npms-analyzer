f5c954d7d09a375880381eeb0a5dad8525abe2d6
André Cruz <andremiguelcruz@msn.com>
2016-04-20 13:51:02 +0100

    Add more tests.

---
+++ test/spec/analyze/util/exec.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const expect = require('chai').expect;
+const exec = require(`${process.cwd()}/lib/analyze/util/exec`);
+
+describe('exec', () => {
+    it('should provide a promise based child_processed#exec', () => {
+        return exec('echo foo')
+        .then(() => {});
+    });
+
+    it('should resolve with an array with `stdout` and `stderr` items', () => {
+        return Promise.resolve()
+        .then(() => {
+            return exec('echo foo')
+            .spread((stdout, stderr) => {
+                expect(stdout).to.equal('foo\n');
+                expect(stderr).to.equal('');
+            });
+        })
+        .then(() => {
+            return exec('echo foo 1>&2')
+            .spread((stdout, stderr) => {
+                expect(stdout).to.equal('');
+                expect(stderr).to.equal('foo\n');
+            });
+        });
+    });
+
+    it('should reject with an error, containing an additional `stdout` and `stderr` properties', () => {
+        return exec('echo foo && exit 1')
+        .then(() => {
+            throw new Error('Should have failed');
+        }, (err) => {
+            expect(err.code).to.equal(1);
+            expect(err.stdout).to.equal('foo\n');
+            expect(err.stderr).to.equal('');
+
+            return exec('echo foo 1>&2 && exit 2')
+            .then(() => {
+                throw new Error('Should have failed');
+            }, (err) => {
+                expect(err.code).to.equal(2);
+                expect(err.stdout).to.equal('');
+                expect(err.stderr).to.equal('foo\n');
+            });
+        });
+    });
+
+    describe('escape', () => {
+        it('should offer an es6 template tag for escaping arguments', () => {
+            const command = exec.escape`echo ${'foo>bar'} ${'| something'}`;
+
+            expect(command).to.equal('echo \'foo>bar\' \'| something\'');
+        });
+    });
+});
---
+++ test/spec/analyze/util/normalizePackageJsom.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const expect = require('chai').expect;
+const normalizePackageJson = require(`${process.cwd()}/lib/analyze/util/normalizePackageJson`);
+
+describe('normalizePackageJson', () => {
+    it('should mutate original object', () => {
+        const packageJson = { name: 'foo' };
+        const normalizedPackageJson = normalizePackageJson(packageJson);
+
+        expect(packageJson).to.equal(normalizedPackageJson);
+    });
+
+    it('should mock version if not present', () => {
+        expect(normalizePackageJson({ name: 'foo' }).version).to.equal('0.0.1');
+    });
+
+    it('should normalize package json', () => {
+        expect(normalizePackageJson({ name: 'foo' }).readme).to.equal('ERROR: No README data found!');
+    });
+
+    it('should throw an unrecoverable error if normalize-package-data crashes', () => {
+        try {
+            normalizePackageJson({
+                name: 'foo',
+                repository: { type: 'git', url: 'git://github.com/balderdashy/waterline-%s.git' },
+            });
+        } catch (err) {
+            expect(err.message).to.match(/uri malformed/i);
+            expect(err.unrecoverable).to.equal(true);
+        }
+    });
+
+    it('should normalize repository trailing slashes', () => {
+        const packageJson = normalizePackageJson({
+            name: 'foo',
+            repository: { type: 'git', url: 'git://github.com/balderdashy/waterline.git/' },
+        });
+
+        expect(packageJson.repository.url).to.equal('git://github.com/balderdashy/waterline.git');
+    });
+});
