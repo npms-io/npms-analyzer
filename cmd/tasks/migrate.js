9de2618d6415580d57d7d10f1dfc149c02a5329f
André Cruz <andremiguelcruz@msn.com>
2017-03-27 01:59:40 +0100

    Update migration.

--- cmd/tasks/migrate.js
+++ cmd/tasks/migrate.js
@@ -1,12 +1,19 @@
 'use strict';
 
 const couchdbIterator = require('couchdb-iterator');
-const couchdbForce = require('couchdb-force');
-const camelcaseKeys = require('camelcase-keys');
+const analyze = require('../../lib/analyze');
 const bootstrap = require('../util/bootstrap');
 const stats = require('../util/stats');
 
 const log = logger.child({ module: 'cli/migrate' });
+
+function extractScope(name) {
+    const match = name.match(/^@([^/]+)\/.+$/);
+
+    return match ? match[1] : 'unscoped';
+}
+
+// --------------------------------------------------------------
 
 exports.command = 'migrate [options]';
 exports.describe = 'Run the latest migration';
@@ -21,82 +28,38 @@ exports.handler = (argv) => {
     logger.level = argv.logLevel || 'info';
 
     // Bootstrap dependencies on external services
-    bootstrap(['couchdbNpms'])
-    .spread((npmsNano) => npmsNano)
-    .tap(() => stats.process())
-    // Migrate module! to package!
-    .tap((npmsNano) => {
-        log.info('Starting module! -> package! migration');
+    bootstrap(['couchdbNpm', 'couchdbNpms'])
+    .spread((npmNano, npmsNano) => {
+        log.info('Starting packages re-metadata');
 
+        // Stats
+        stats.process();
+
+        // Iterate over all packages
         return couchdbIterator(npmsNano, (row) => {
-            row.index && row.index % 10000 === 0 && log.info(`Processed ${row.index} rows`);
+            row.index && row.index % 2500 === 0 && log.info(`Processed ${row.index} rows`);
 
-            const doc = row.doc;
-
-            if (!doc) {
+            if (!row.doc) {
                 return;
             }
 
-            const newDoc = Object.assign({}, doc, { _id: doc._id.replace(/^module!/, 'package!') });
+            const name = row.doc.collected.metadata.name;
 
-            // dependenciesVulnerabilities -> vulnerabilities
-            if (newDoc.collected.source && newDoc.collected.source.dependenciesVulnerabilities) {
-                newDoc.collected.source.vulnerabilities = newDoc.collected.source.dependenciesVulnerabilities
-                .map((vulnerability) => camelcaseKeys(vulnerability, { deep: true }));
-                delete newDoc.collected.source.dependenciesVulnerabilities;
-            }
+            row.doc.collected.metadata.scope = extractScope(name);
 
-            // dependenciesHealth -> health
-            if (newDoc.evaluation.quality.dependenciesHealth != null) {
-                newDoc.evaluation.quality.health = newDoc.evaluation.quality.dependenciesHealth;
-                delete newDoc.evaluation.quality.dependenciesHealth;
-            }
-
-            return couchdbForce.insert(npmsNano, newDoc)
-            .then(() => npmsNano.destroyAsync(doc._id, doc._rev))
-            .then(() => log.debug(`Migrated ${doc._id}..`));
+            return analyze.save(row.doc, npmsNano)
+            .catch((err) => {
+                log.error({ err }, `Failed to process ${name}`);
+                throw err;
+            });
         }, {
-            startkey: 'module!',
-            endkey: 'module!\ufff0',
+            startkey: 'package!',
+            endkey: 'package!\ufff0',
             concurrency: 25,
             limit: 2500,
             includeDocs: true,
         })
         .then((count) => log.info(`Completed, processed a total of ${count} rows`));
-    })
-    // Migrate observer!module to observer!package
-    .tap((npmsNano) => {
-        log.info('Starting observer!module! -> observer!package! migration');
-
-        return couchdbIterator(npmsNano, (row) => {
-            row.index && row.index % 10000 === 0 && log.info(`Processed ${row.index} rows`);
-
-            const doc = row.doc;
-
-            if (!doc) {
-                return;
-            }
-
-            const newDoc = Object.assign({}, doc, { _id: doc._id.replace(/^observer!module!/, 'observer!package!') });
-
-            return couchdbForce.insert(npmsNano, newDoc)
-            .then(() => npmsNano.destroyAsync(doc._id, doc._rev))
-            .then(() => log.debug(`Migrated ${doc._id}..`));
-        }, {
-            startkey: 'observer!module!',
-            endkey: 'observer!module!\ufff0',
-            concurrency: 25,
-            limit: 2500,
-            includeDocs: true,
-        })
-        .then((count) => log.info(`Completed, processed a total of ${count} rows`));
-    })
-    // Migrate design doc
-    .tap((npmsNano) => {
-        log.info('Migrating design doc..');
-
-        return couchdbForce.insert(npmsNano, require('../../config/couchdb/npms-analyzer.json'))  // eslint-disable-line global-require
-        .then(() => log.info('Migrated design doc'));
     })
     .then(() => process.exit())
     .done();
