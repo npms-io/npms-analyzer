d8b392bdca6a17bb9843a469d92f1a00274c131a
André Cruz <andremiguelcruz@msn.com>
2017-04-03 22:35:04 +0100

    Prefer downloaded package.json props if the original ones are empty.

--- test/spec/analyze/download/util/mergePackageJson.js
+++ test/spec/analyze/download/util/mergePackageJson.js
@@ -17,6 +17,34 @@ describe('mergePackageJson', () => {
             version: '2.0.0',
             description: 'bar',
             dependencies: { 'foo-dep': '^1.0.0' },
+            scripts: {},
+        }));
+
+        const packageJson = {
+            name: 'cool-module',
+            version: '1.0.0',
+            keywords: ['cool'],
+            dependencies: { 'cool-dep': '^1.0.0' },
+            scripts: { test: 'mocha' },
+        };
+
+        return mergePackageJson(packageJson, tmpDir)
+        .then(() => {
+            expect(packageJson.name).to.equal('cool-module');
+            expect(packageJson.version).to.equal('1.0.0');
+            expect(packageJson.description).to.equal('bar');
+            expect(packageJson.keywords).to.eql(['cool']);
+            expect(packageJson.dependencies).to.eql({ 'cool-dep': '^1.0.0' });
+            expect(packageJson.scripts).to.eql({ test: 'mocha' });
+        });
+    });
+
+    it('should merge the files, preferring the passed one', () => {
+        fs.writeFileSync(`${tmpDir}/package.json`, JSON.stringify({
+            name: 'cool-module',
+            version: '2.0.0',
+            description: 'bar',
+            dependencies: { 'foo-dep': '^1.0.0' },
         }));
 
         const packageJson = {
@@ -35,6 +63,7 @@ describe('mergePackageJson', () => {
             expect(packageJson.dependencies).to.eql({ 'cool-dep': '^1.0.0' });
         });
     });
+
 
     it('should deal with broken downloaded package.json files', () => {
         fs.writeFileSync(`${tmpDir}/package.json`, 'brokenjson');
